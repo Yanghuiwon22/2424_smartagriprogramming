4ccc28e8f7ae225d552651bb07700e631b570b48
ppigguritta <pnh020424@naver.com>
2024-09-15 17:17:40 +0900

    Api 데이터 받아오기

---
+++ hw03/project_info_down.py
@@ -0,0 +1,12 @@
+import requests
+import pprint
+import json
+
+service_key = "cnFWOksdH2rQuZ9YQs2IR3frMjm2kgy8eauRY4ujdTSTvGEeDGXulTzCIJtU7htSZeFnoof4l6RGh3E"
+# url = ("http://apis.data.go.kr/1390802/AgriWeather/WeatherObsrInfo/V2/GnrlWeather/getWeatherTenMinList?serviceKey={service_key}&Page_No=1&Page_Size=20&date=2018-01-01&obsr_Spot_Nm=가평군 가평읍&obsr_Spot_Code=477802A001")
+url = f"https://apis.data.go.kr/1390802/AgriWeather/WeatherObsrInfo/V2/GnrlWeather/getWeatherTenMinList?serviceKey={service_key}&Page_No=1&Page_Size=20&date=2018-01-01&time=1300&obsr_Spot_Nm=%EA%B0%80%ED%8F%89%EA%B5%B0%20%EA%B0%80%ED%8F%89%EC%9D%8D&obsr_Spot_Code=477802A001"
+
+response =requests.get(url)
+content =response.text
+
+print(response.content)
