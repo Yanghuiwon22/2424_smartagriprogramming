a49438269a016a1e83d09808c1bdcab1a124a060
ppigguritta <pnh020424@naver.com>
2024-09-19 21:29:41 +0900

    그래프 그리기 (임시 데이터 이용)

---
+++ hw03/gps_location.py
@@ -0,0 +1,34 @@
+import uvicorn
+from fastapi import FastAPI
+from markupsafe import escape
+import requests
+
+app= FastAPI()
+
+# key =  7F232D24-1336-390E-9B68-F17F45877199
+url = "https://api.vworld.kr/req/address?"
+
+params = {
+	"service": "address",
+	"request": "getcoord",
+	"crs": "epsg:4326",
+	"address": "판교로 242",
+	"format": "json",
+	"type": "road",
+	"key": "7F232D24-1336-390E-9B68-F17F45877199"
+}
+
+response = requests.get(url, params=params)
+if response.status_code == 200:
+	print(response.json())
+@app.get("/")
+def root():
+    return f'Hello World'
+
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+
+
--- hw03/project_info_down.py
+++ hw03/project_info_down.py
@@ -4,17 +4,22 @@ import json
 import xmltodict
 
 #요청인자
+
 page_no = "1"
 page_size ="20"
-date = "2024-01-01"
+date = "2024-01-11"
 time= "1300"
-spot_nm ="전주시 송천동"
+spot_nm = ""
 spot_code = ""
 
-#url, key
+# url, key
 service_key = "wIdqPjRtIkrKsaya4kGkAD%2Bo8FsV1GsN4rIyX6ntn7GlYSZr%2FgP%2FtVa1ZDeRcP04jDjy93oziypc5RMFwFM4Mg%3D%3D"
 
-url =f"http://apis.data.go.kr/1390802/AgriWeather/WeatherObsrInfo/V2/GnrlWeather/getWeatherTenMinList?serviceKey={service_key}&Page_No={page_no}&Page_Size={page_size}&date={date}&time={time} & obsr_Spot_Nm={spot_nm}&obsr_Spot_Code={spot_code}"
+url =(f"http://apis.data.go.kr/1390802/AgriWeather/WeatherObsrInfo/V2/GnrlWeather/getWeatherTenMinList?"
+      f"serviceKey={service_key}&Page_No={page_no}&Page_Size={page_size}&"
+      f"date={date}&time={time}&obsr_Spot_Nm={spot_nm}&obsr_Spot_Code={spot_code}")
+
+
 
 response =requests.get(url)
 content =response.text
@@ -22,5 +27,9 @@ content =response.text
 xml_dict = xmltodict.parse(content)
 # json_data = json.dumps(xml_dict)
 
-content = xml_dict['response']['body']['items']['item'][0]
+content = xml_dict['response']['body']['items']['item']
 print(content)
+
+
+
+
