b02ee93621f1df4737d5ad052eaca32f7fba23ec
ppigguritta <pnh020424@naver.com>
2024-09-29 21:07:02 +0900

    DVR 모델 식

---
+++ flower_modeling.py
@@ -0,0 +1,11 @@
+# DVR model
+
+pear_a = 107.94
+pear_b = 0.9
+
+# 일평균
+day_temp = []
+
+DVRi = 1/(pear_a *(pear_b**day_temp))
+
+#DVRi 누적값 100 -> 예상 만개기
--- hw03/weather_visual.py
+++ hw03/weather_visual.py
@@ -100,6 +100,106 @@ def api_get(start_date, spot_code):
             print(end_date)
             date = end_date.strftime("%Y-%m-%d")
             current_time = start_time
+import requests
+import xmltodict
+from concurrent.futures import ThreadPoolExecutor, as_completed
+from datetime import datetime, timedelta
+import pandas as pd
+from matplotlib import pyplot as plt
+from matplotlib.patches import Patch
+import numpy as np
+
+# 요청 인자
+page_no = "1"
+page_size = "20"
+spot_nm = ""
+spot_code = ""
+
+# key
+service_key = "cnFWOksdH2rQuZ9YQs2IR3frMjm2kgy8eauRY4ujdTSTvGEeDGXulTzCIJtU7htSZeFnoof4l6RGh3EpVIbo1Q%3D%3D"
+
+# 시작 날짜 및 기간
+# start_date = datetime(2024, 1, 10)
+days = 7
+time_format = "%H%M"
+start_time = datetime.strptime("0010", time_format)
+
+# 결과를 저장할 리스트
+results = []
+def get_button(address_input):
+    address_do = address_input.split(' ')[0]
+    address_si = address_input.split(' ')[1]
+
+    df = pd.read_csv('static/df_after.csv')
+    try:
+        if address_do in df['도명'].values:
+            if address_si in df['지점명1'].values:
+                button_row = df[df['지점명1'] == address_si]  # --> 조건에 맞는 행의 수 구하기
+                return button_row
+
+    except:
+        pass
+
+def get_address(address_input):
+    address_input = '경상북도 구미시'
+
+    address_do = address_input.split(' ')[0]
+    address_si = address_input.split(' ')[1]
+
+    df = pd.read_csv('static/df_after.csv')
+    try:
+        if address_do in df['도명'].values:
+            if address_si in df['지점명1'].values:
+
+                row = df[df['지점명1'] == address_si] # --> 조건에 맞는 행의 수 구하기
+                buttons = [f"{df['지점명2'][i]}" for i in row.index]
+                index = df[df['지점명1'] == address_si].index
+                return (buttons)
+            else:
+                return f'{address_si}에 대한 데이터 없습니다.'
+        else:
+            print(f'{address_do}에 대한 데이터 없습니다')
+            return f'{address_do}에 대한 데이터 없습니다'
+    except:
+        return '로드 실패 : 관리자에게 문의'
+
+def fetch_weather_data(date, time, spot_code):
+
+    """날짜와 시간을 받아 API로부터 데이터를 가져오는 함수"""
+    if time == '0000':
+        time = "2400"
+    url = f"http://apis.data.go.kr/1390802/AgriWeather/WeatherObsrInfo/V2/GnrlWeather/getWeatherTenMinList?serviceKey={service_key}&Page_No={page_no}&Page_Size={page_size}&date={date}&time={time}&obsr_Spot_Nm={spot_nm}&obsr_Spot_Code={spot_code}"
+    response = requests.get(url)
+    content = response.text
+    xml_dict = xmltodict.parse(content)
+
+    # 필요한 데이터 추출
+    if 'response' in xml_dict and 'body' in xml_dict['response']:
+        content = xml_dict['response']['body']['items']['item']
+        date__ = content['date'].split(' ')[0]
+        time__ = content['date'].split(' ')[1]
+
+        # print(date__, time__, content['temp'])
+        if time__ == "24:00":
+            time__ = "00:00"
+            date__ = (datetime.strptime(date__, "%Y-%m-%d") + timedelta(days=1)).strftime("%Y-%m-%d")
+
+            return (date__, time__, content['temp'], content['hum'], content['wind'])
+
+        else:
+            return (date__, time__, content['temp'], content['hum'], content['wind'])
+
+
+def api_get(start_date, spot_code):
+# 병렬 처리로 데이터를 가져옴
+    start_date = datetime.strptime(start_date, "%Y-%m-%d")
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        futures = []
+        for i in range(days):
+            end_date = start_date - timedelta(days=i)
+            print(end_date)
+            date = end_date.strftime("%Y-%m-%d")
+            current_time = start_time
 
             # 10분 간격으로 날씨 데이터를 가져오는 작업을 병렬로 처리
             for j in range(24 * 7):
