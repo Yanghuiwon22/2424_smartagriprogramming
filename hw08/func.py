2869ed01e345d148e48028a101ea093e140ff1b2
ppigguritta <pnh020424@naver.com>
2024-10-23 17:11:08 +0900

    기상대에서 데이터 받아오기

--- hw08/func.py
+++ hw08/func.py
@@ -1,22 +1,79 @@
 import requests
 import concurrent.futures
+import pandas as pd
+from datetime import datetime, timedelta, timezone
 
+# 필드 이름
+field_names = {
+    1: '온도',
+    2: '습도',
+    3: '일사량',
+    4: '풍향',
+    5: '풍속',
+    6: '강우',
+    7: '베터리 전압'
+}
+
+# 데이터를 가져오는 함수
 def fetch_data(i):
     url = f'https://thingspeak.mathworks.com/channels/2328695/field/{i}.json'
-    response = requests.get(url)
-    if response.status_code == 200:
-        return i, response.json()['feeds'][-1]
-    return i, None
+    try:
+        response = requests.get(url)
+        response.raise_for_status()  # 상태 코드가 200이 아닐 때 예외 발생
+        data = response.json()
+        if 'feeds' in data and data['feeds']:
+            # 24시간 이내 데이터 필터링 (현재 시각 - 24시간)
+            latest_data = []
+            now = datetime.now(timezone(timedelta(hours=9)))
+            time_24_hours_ago = now - timedelta(hours=24)
 
+            for feed in data['feeds']:
+                utc_time_str = feed['created_at']
+
+                utc_time = datetime.strptime(utc_time_str, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=timezone.utc)
+                kst_time = utc_time.astimezone(timezone(timedelta(hours=9)))
+
+                # KST 시간 형식으로 변환 (문자열)
+                kst_time_str = kst_time.strftime("%Y-%m-%d %H:%M")
+
+                if kst_time >= time_24_hours_ago:
+                    latest_data.append({
+                        'Date': kst_time_str,
+                        field_names[i]: feed[f'field{i}']
+                    })
+            return latest_data if latest_data else None
+        else:
+            return None
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching data for field {i}: {e}")
+        return None
+
+# 각 필드 데이터를 가져오는 함수
 def jbnu_aws_data():
-    result = {}
+    result = []
     with concurrent.futures.ThreadPoolExecutor() as executor:
         futures = [executor.submit(fetch_data, i) for i in range(1, 8)]
         for future in concurrent.futures.as_completed(futures):
-            i, data = future.result()
+            data = future.result()
             if data:
-                result[i] = data
+                result.extend(data)
 
     return result
-jbnu_aws_data()
 
+
+# 데이터프레임 변환
+def data_to_dataframe():
+    data = jbnu_aws_data()
+    if data:
+        df = pd.DataFrame(data)
+
+        df = df.groupby('Date', as_index=False).first()
+        # 칼럼 순서 재정렬
+        df = df[['Date'] + [field_names[i] for i in range(1, 8)]]
+        print(df)
+    else:
+        print("No data available.")
+
+
+if __name__ == "__main__":
+    data_to_dataframe()
