2b4eda5d9c7618b9ed3f3dade8093aabd203596b
ppigguritta <pnh020424@naver.com>
2024-10-28 11:09:01 +0900

    .

--- hw08/func.py
+++ hw08/func.py
@@ -65,11 +65,11 @@ def jbnu_aws_data():
         df = df[['Date'] + [field_names[i] for i in range(1, 8)]]
         print(df)
 
-        # CSV 파일로 저장 (덮어쓰기 모드)
-        # hw08 디렉토리가 없다고 나옴,,? -> 다시 해보기..ㅠㅜ
-        df.to_csv('/hw08/output_data.csv', index=False, encoding='utf-8-sig')
-
-        print("Data saved to hw08/output_data.csv")
+        # # CSV 파일로 저장 (덮어쓰기 모드)
+        # # hw08 디렉토리가 없다고 나옴,,? -> 다시 해보기..ㅠㅜ
+        # df.to_csv('/hw08/output_data.csv', index=False, encoding='utf-8-sig')
+        #
+        # print("Data saved to hw08/output_data.csv")
         return df
 
     else:
