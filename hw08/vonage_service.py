eb84380b2024e83137a3828a1dc74af32bae4989
ppigguritta <pnh020424@naver.com>
2024-11-03 02:43:26 +0900

    .

--- hw08/vonage_service.py
+++ hw08/vonage_service.py
@@ -8,12 +8,11 @@ load_dotenv()  # .env 파일 로드
 api_key = os.getenv("VONAGE_API_KEY")
 secret_key = os.getenv("VONAGE_SECRET")
 
-def send_sms(message, url):
+def send_sms(message):
     client = vonage.Client(key=api_key, secret=secret_key)
 
     message_df = pd.read_csv(os.path.join(FILE_PATH, 'message_number.csv'))
-    full_message = f"{message}\n{url}"
-    print(message_df['number'])
+
 
     for index, row in message_df.iterrows():
         if row['조건'] == 'default':
@@ -24,7 +23,7 @@ def send_sms(message, url):
     #     response = client.sms.send_message({
     #         'from': 'VonageSMS',
     #         'to': f'+82{user['number']}',
-    #         'text': full_message,
+    #         'text': '전북대학교 기상대 모니터링 서비스입니다',
     #         'type': 'unicode'  # UCS-2 인코딩을 사용
     #     })
     #
@@ -35,8 +34,5 @@ def send_sms(message, url):
 
 
 if __name__ == '__main__':
-    message_content = "전북대학교 기상대 모니터링 서비스입니다"
-    url_to_send = "https://yanghuiwon22-hw08.streamlit.app/"  # 보내고자 하는 URL
-
-    send_sms(message_content, url_to_send)  # 수신자 번호, 메시지 내용, URL 입력
+    send_sms()
 
