2cb75a28596be9ef649bf550c4795976bac49a9e
ppigguritta <pnh020424@naver.com>
2024-12-15 20:40:43 +0900

    ,,,휴,,,,디자인 넘 어렵당,ㅠㅜ,아이디어 고갈

--- final_project/input_data.py
+++ final_project/input_data.py
@@ -7,9 +7,9 @@ def get_sensor_data():
     """
     return {
         "image_url": "https://example.com/crop_image.jpg",
-        "temperature": round(random.uniform(20, 30), 2),  # 임의의 온도 데이터
-        "humidity": round(random.uniform(40, 60), 2),    # 임의의 습도 데이터
-        "water_level": round(random.uniform(10, 100), 2),  # 임의의 물의 양 데이터
+        "temperature": round(random.uniform(20, 30), 1),  # 임의의 온도 데이터
+        "humidity": round(random.uniform(40, 60), 1),    # 임의의 습도 데이터
+        "water_level": round(random.uniform(10, 100), 1),  # 임의의 물의 양 데이터
         "irrigation_status": "ON" if random.choice([True, False]) else "OFF"  # 관수 상태
     }
 #def draw_graph():
