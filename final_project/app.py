5bf2300ef23c66b15d0f7d7d36beabc9fbd2fd05
Yanghuiwon22 <gmldnjs4212@naver.com>
2024-12-20 19:04:03 +0900

    3. 초음파센서 대시보드 자동 업데이트

--- final_project/app.py
+++ final_project/app.py
@@ -1,11 +1,28 @@
 from flask import Flask, render_template, jsonify
 from input_data import generate_fake_data, get_latest_sensor_data
-
+import requests
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    data = get_water_distance()
+    return render_template('index.html', data=data)
+
+# 센서 받아오기
+# 1. 온습도
+# 2. 로드셀
+
+# 3. 수위센서
+@app.route('/sensor-data', methods=['GET'])
+def get_water_distance():
+    print("수위센서 데이터를 가져옵니다.")
+    url = "http://192.168.50.243:5000/temp_hum"
+    response = requests.get(url)
+    data = response.json()
+    if response.status_code == 200:
+        return data
+    else:
+        return jsonify({"error": "센서 데이터를 가져오지 못했습니다."})
 
 @app.route('/api/sensor_data')
 def sensor_data():
@@ -14,6 +31,9 @@ def sensor_data():
     """
     fake_data = generate_fake_data()
     latest_data = get_latest_sensor_data(fake_data)
+
+    # 센서 데이터 업데이트 (실시간)
+    get_water_distance()
     return jsonify(latest_data)
 
 @app.route('/api/sensor_history')
