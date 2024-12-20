60f2ac181b856a25f96f038a02e50d1cac41505a
Yanghuiwon22 <gmldnjs4212@naver.com>
2024-12-20 22:26:34 +0900

    완료 - 수위

--- final_project/app.py
+++ final_project/app.py
@@ -7,8 +7,8 @@ app = Flask(__name__)
 def home():
     # print("수위센서 데이터를 가져옵니다.")
     # return render_template('index.html')
-    # data = get_water_distance()
-    return render_template('index.html')
+    data = get_water_distance()
+    return render_template('index.html', data=data)
 
 # 센서 받아오기
 # 1. 온습도
@@ -30,27 +30,5 @@ def get_water_distance():
         print(f"에러 발생: {e}")
         return jsonify({"error": "센서 데이터를 가져오지 못했습니다."}), 500
 
-# get_water_distance()
-#
-# @app.route('/api/sensor_data')
-# def sensor_data():
-#     """
-#     센서 데이터 API 엔드포인트 (가장 최근 데이터 제공)
-#     """
-#     fake_data = generate_fake_data()
-#     latest_data = get_latest_sensor_data(fake_data)
-#
-#     # 센서 데이터 업데이트 (실시간)
-#     get_water_distance()
-#     return jsonify(latest_data)
-#
-# @app.route('/api/sensor_history')
-# def sensor_history():
-#     """
-#     센서 데이터 API 엔드포인트 (전체 데이터 제공)
-#     """
-#     fake_data = generate_fake_data()
-#     return jsonify(fake_data)
-#
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True, port=8000)
